2a338dbe0f3084c9e137de9294ead5b831a78fdd
Benson306 <bnkimtai@gmail.com>
2023-12-13 11:28:25 +0300

    Influencer approval

--- Controllers/CreatorApplicationsController.js
+++ Controllers/CreatorApplicationsController.js
@@ -49,11 +49,51 @@ app.get('/content_creator_applicants', async (req, res)=>{
     }
 })
 
+app.get('/influencer_creator_applicants', async (req, res)=>{
+
+    try {
+        const creatorProfiles = await CreatorProfileModel.find({ creatorType: 'influencer', isComplete: true });
+
+        if (creatorProfiles) {
+            const completeData = [];
+
+            for (const item of creatorProfiles) {
+                if (item.isApproved == "0") {
+                    completeData.push({
+                        _id: item._id,
+                        email: item.email,
+                        firstName: item.firstName,
+                        lastName: item.lastName,
+                        countryCode: item.countryCode,
+                        phoneNumber: item.phoneNumber,
+                        industries: item.industries
+                    });
+                }
+            }
+
+            res.status(200).json(completeData);
+        } else {
+            res.status(404).json("Not Found");
+        }
+    } catch (err) {
+        res.status(500).json('failed');
+    }
+})
 
 
 app.post('/approve/content/:id', urlEncoded, (req, res)=>{
 
     CreatorProfileModel.findOneAndUpdate({_id: req.params.id}, { isApproved: req.body.value}, { new: true})
+    .then(()=>{
+        res.status(200).json('success');
+    })
+    .catch(err => {
+        res.status(500).json('failed');
+    })
+})
+
+app.get('/approve/influencer/:type/:id', urlEncoded, (req, res)=>{
+    CreatorProfileModel.findOneAndUpdate({_id: req.params.id}, { isApproved: req.params.type}, { new: true})
     .then(()=>{
         res.status(200).json('success');
     })
